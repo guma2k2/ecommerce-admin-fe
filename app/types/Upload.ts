98d7fa2641265943b9131eb74ebc22c246a262b7
Thuan Ngo | Dev <thuan.ngo@689cloud.asia>
2026-01-06 23:42:50 +0700

    Update: - Modify Input File component

---
+++ app/types/Upload.ts
@@ -0,0 +1,9 @@
+export type UploadStatus = 'idle' | 'uploading' | 'success' | 'error'
+
+export type UploadType = {
+  file: File | null
+  progress: number
+  status: UploadStatus
+  url: string
+  id: string
+}
