039197bce8998516b0e3833c5b73cc973f57290f
Thuan Ngo | Dev <thuan.ngo@689cloud.asia>
2026-03-04 14:10:25 +0700

    Update: - Modify upload component

--- app/pages/authenticate/admin/manageProduct/CreateProductPage.tsx
+++ app/pages/authenticate/admin/manageProduct/CreateProductPage.tsx
@@ -1,70 +1,7 @@
-import { useState } from 'react'
-import ProductInformationForm from '~/features/authenticate/manageProduct/components/ProductInformationForm'
-import ProductVariantForm from '~/features/authenticate/manageProduct/components/ProductVariantForm'
+import ProductInformationForm from "~/features/authenticate/manageProduct/components/ProductInformationForm"
+import ProductVariantForm from "~/features/authenticate/manageProduct/components/ProductVariantForm"
 
 export default function CreateProductPage() {
-  const [images, setImages] = useState<{ id: string; url: string; position?: number }[]>([
-    {
-      id: 'img-1',
-      url: 'https://plus.unsplash.com/premium_photo-1766408195454-7fc26af72ddf?w=600&auto=format&fit=crop&q=60',
-      position: 1
-    },
-    {
-      id: 'img-2',
-      url: 'https://images.unsplash.com/photo-1766258725835-88ff59f0040d?w=600&auto=format&fit=crop&q=60',
-      position: 2
-    },
-    {
-      id: 'img-3',
-      url: 'https://images.unsplash.com/photo-1766425597345-629c0f803d4c?w=600&auto=format&fit=crop&q=60',
-      position: 3
-    },
-    {
-      id: 'img-4',
-      url: 'https://images.unsplash.com/photo-1766488735864-44c313801587?w=600&auto=format&fit=crop&q=60',
-      position: 4
-    },
-    {
-      id: 'img-5',
-      url: 'https://plus.unsplash.com/premium_photo-1766408195454-7fc26af72ddf?w=600&auto=format&fit=crop&q=60',
-      position: 5
-    },
-    {
-      id: 'img-6',
-      url: 'https://images.unsplash.com/photo-1766258725835-88ff59f0040d?w=600&auto=format&fit=crop&q=60',
-      position: 6
-    },
-    {
-      id: 'img-7',
-      url: 'https://images.unsplash.com/photo-1766425597345-629c0f803d4c?w=600&auto=format&fit=crop&q=60',
-      position: 7
-    },
-    {
-      id: 'img-8',
-      url: 'https://images.unsplash.com/photo-1766488735864-44c313801587?w=600&auto=format&fit=crop&q=60',
-      position: 8
-    },
-    {
-      id: 'img-9',
-      url: 'https://images.unsplash.com/photo-1766425597345-629c0f803d4c?w=600&auto=format&fit=crop&q=60',
-      position: 9
-    },
-    {
-      id: 'img-10',
-      url: 'https://images.unsplash.com/photo-1766488735864-44c313801587?w=600&auto=format&fit=crop&q=60',
-      position: 10
-    },
-    {
-      id: 'img-11',
-      url: 'https://images.unsplash.com/photo-1766425597345-629c0f803d4c?w=600&auto=format&fit=crop&q=60',
-      position: 11
-    },
-    {
-      id: 'img-12',
-      url: 'https://images.unsplash.com/photo-1766488735864-44c313801587?w=600&auto=format&fit=crop&q=60',
-      position: 12
-    }
-  ])
   return (
     <div className='w-full bg-gray-100 h-full'>
       <div className='w-5xl mx-auto p-4'>
